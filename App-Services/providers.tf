c3f26d34b37e2545c81887f1f2dea701795e84c4
gbhavika97 <92913843+gbhavika97@users.noreply.github.com>
2022-01-31 13:03:14 +0530

    Add files via upload

---
+++ App-Services/providers.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {}
+}
+
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "testrg"
+    storage_account_name = "healthcareus3"
+    container_name       = "terraform"
+    key                  = "dev.tfstate"
+  }
+}
---
+++ App-Services/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+    type = string
+    description = "Resource Group Name in Azure"
+}
+
+variable "resource_group_location" {
+    type = string
+    description = "Resource Group Location in Azure"
+}
+
+variable "app_service_plan_name" {
+    type = string
+    description = "App Service Plan Name in Azure"
+}
+
+variable "app_service_name" {
+    type = string
+    description = "App Service Name in Azure"
+}
